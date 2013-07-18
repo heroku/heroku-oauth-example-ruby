4a6a64e1884a394b1beb6a89b272aa7f744ebe37
Brandur <brandur@mutelight.org>
2013-07-18 14:51:35 -0700

    Use identity scope

--- web.rb
+++ web.rb
@@ -9,7 +9,7 @@ require "omniauth-heroku"
 
 use Rack::Session::Cookie, :secret => ENV["COOKIE_SECRET"]
 use OmniAuth::Builder do
-  provider :heroku, ENV["HEROKU_OAUTH_ID"], ENV["HEROKU_OAUTH_SECRET"]
+  provider :heroku, ENV["HEROKU_OAUTH_ID"], ENV["HEROKU_OAUTH_SECRET"], { scope: "identity" }
 end
 
 get "/" do
